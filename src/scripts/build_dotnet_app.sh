2a427c60916a4f90f5e100ba93a0fdef7ce046a7
Bozhidar Stanishovski <bozidarbt@gmail.com>
2021-12-13 15:18:03 +0100

    added build command

---
+++ src/scripts/build_dotnet_app.sh
@@ -0,0 +1,6 @@
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+
+export DOTNET_APP_DIRECTORY="$PARAM_DOTNET_APP_DIRECTORY"
+
+$SUDO dotnet publish -o "$DOTNET_APP_DIRECTORY"
+$SUDO zip -r "$DOTNET_APP_DIRECTORY"_"$CIRCLE_SHA1".zip "$DOTNET_APP_DIRECTORY"
