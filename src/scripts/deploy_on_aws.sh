c0d25fcee2dfe2a2fc22b52937897b4b5d62bb95
Bozhidar Stanishovski <bozidarbt@gmail.com>
2021-12-14 13:30:06 +0100

    changed sh

--- src/scripts/deploy_on_aws.sh
+++ src/scripts/deploy_on_aws.sh
@@ -1,14 +1,14 @@
 if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
 export DOTNET_APP_DIRECTORY="$PARAM_DOTNET_APP_DIRECTORY"
-export ZIP_DIR=${DOTNET_APP_DIRECTORY}_${CIRCLE_SHA1}
-export SSH_LOGIN=${EC2_USERNAME}_${EC2_PUBLIC_DNS}
+export ZIP_DIR="${DOTNET_APP_DIRECTORY}_${CIRCLE_SHA1}"
+export SSH_LOGIN="${EC2_USERNAME}_${EC2_PUBLIC_DNS}""
 
 $SUDO pwd
 $SUDO ls -la
 $SUDO echo "$ZIP_DIR"
 $SUDO ls -l
-export PUBLIC_IP=`curl ipinfo.io/ip`
+PUBLIC_IP=$(curl ipinfo.io/ip)
 echo "$PUBLIC_IP"
 $SUDO bin/aws ec2 authorize-security-group-ingress --region "$AWS_DEFAULT_REGION" --group-id "$SG_ID" --protocol tcp --port 22 --cidr "$PUBLIC_IP"/32
 $SUDO sleep 5
