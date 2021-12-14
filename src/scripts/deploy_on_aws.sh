a2af2f26bba03cb21737dbd8f75a277162a7d63b
bozhos <95410223+bozhos@users.noreply.github.com>
2021-12-14 14:34:57 +0100

    [semver:minor] changed aws cli (#17)
    * [semver:minor] new version with deploy
    
    * [semver:minor] test
    
    * test 4
    
    * changed sh
    
    * fixed errors
    
    * test 5
    
    * test 6
    
    * added zip
    
    * fixed trailing spaces
    
    * [semver:major] added deploy
    
    * [semver:patch] changed aws security group
    
    * [semver:minor] test
    
    * [semver:minor] changed aws location
    
    * [semver:minor] test
    
    * [semver:minor] added aws sh

--- src/scripts/deploy_on_aws.sh
+++ src/scripts/deploy_on_aws.sh
@@ -10,10 +10,10 @@ $SUDO echo "$ZIP_DIR"
 $SUDO ls -l
 PUBLIC_IP=$(curl ipinfo.io/ip)
 echo "$PUBLIC_IP"
-$SUDO bin/aws ec2 authorize-security-group-ingress --region "$AWS_DEFAULT_REGION" --group-id "$SG_ID" --protocol tcp --port 22 --cidr "$PUBLIC_IP"/32
+#$SUDO ~/bin/aws ec2 authorize-security-group-ingress --region "$AWS_DEFAULT_REGION" --group-id "$SG_ID" --protocol tcp --port 22 --cidr "$PUBLIC_IP"/32
+~/bin/aws ec2 authorize-security-group-ingress --region "$AWS_DEFAULT_REGION" --group-id "$SG_ID" --protocol tcp --port 22 --cidr "$PUBLIC_IP"/32
 $SUDO sleep 5
 $SUDO ssh -o StrictHostKeyChecking=no "$SSH_LOGIN"  "sudo systemctl stop test-app.service"
 $SUDO cat "$ZIP_DIR" | ssh -o StrictHostKeyChecking=no "$SSH_LOGIN" "cat > $ZIP_DIR"
 $SUDO ssh -o StrictHostKeyChecking=no "$SSH_LOGIN" "sudo unzip -o $ZIP_DIR -d /opt/"
 $SUDO ssh -o StrictHostKeyChecking=no "$SSH_LOGIN"  "sudo systemctl start test-app.service"
-
