f84fbbe3723ec052f549d5a75052f0c6b7981cab
Bozhidar Stanishovski <bozidarbt@gmail.com>
2021-12-14 15:02:17 +0100

    [semver:patch] changed EC2username with ubuntu

--- src/scripts/deploy_on_aws.sh
+++ src/scripts/deploy_on_aws.sh
@@ -2,7 +2,7 @@ if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
 export DOTNET_APP_DIRECTORY="$PARAM_DOTNET_APP_DIRECTORY"
 export ZIP_DIR="${DOTNET_APP_DIRECTORY}_${CIRCLE_SHA1}.zip"
-export SSH_LOGIN="${EC2_USERNAME}_${EC2_PUBLIC_DNS}"
+export SSH_LOGIN="ubuntu@${EC2_PUBLIC_DNS}"
 
 $SUDO pwd
 $SUDO ls -la
@@ -14,6 +14,7 @@ echo "$PUBLIC_IP"
 whoami
 $SUDO ~/bin/aws ec2 authorize-security-group-ingress --region "$AWS_DEFAULT_REGION" --group-id "$SG_ID" --protocol tcp --port 22 --cidr "$PUBLIC_IP"/32
 $SUDO sleep 5
+echo "$SSH_LOGIN"
 $SUDO ssh -o StrictHostKeyChecking=no "$SSH_LOGIN"  "sudo systemctl stop test-app.service"
 $SUDO cat "$ZIP_DIR" | ssh -o StrictHostKeyChecking=no "$SSH_LOGIN" "cat > $ZIP_DIR"
 $SUDO ssh -o StrictHostKeyChecking=no "$SSH_LOGIN" "sudo unzip -o $ZIP_DIR -d /opt/"
