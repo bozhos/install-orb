76385e459ae0265ac1a9714c95f6877949f5c954
Bozhidar Stanishovski <bozidarbt@gmail.com>
2021-12-15 10:33:51 +0100

    [semver:minor] test

--- src/scripts/build_dotnet_app.sh
+++ src/scripts/build_dotnet_app.sh
@@ -2,7 +2,6 @@ if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
 export DOTNET_APP_DIRECTORY="$PARAM_DOTNET_APP_DIRECTORY"
 
-
 $SUDO pwd
 $SUDO ls
 $SUDO dotnet publish -o "$DOTNET_APP_DIRECTORY"
