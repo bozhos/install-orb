c80ccae35bb161f6772d4e6829526fcfb9a6c4ef
Bozhidar Stanishovski <bozidarbt@gmail.com>
2021-12-15 09:26:36 +0100

    [semver:patch] removed ssh

--- src/scripts/deploy_on_aws.sh
+++ src/scripts/deploy_on_aws.sh
@@ -16,13 +16,5 @@ whoami
 $SUDO ~/bin/aws configure set aws_access_key_id "$AWS_ACCESS_KEY" --profile "$AWS_PROFILE"
 $SUDO ~/bin/aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile "$AWS_PROFILE"
 $SUDO ~/bin/aws ec2 authorize-security-group-ingress --region "$AWS_DEFAULT_REGION" --group-id "$SG_ID" --protocol tcp --port 22 --cidr "$PUBLIC_IP"/24
-$SUDO sleep 5
-echo "$EC2_PUBLIC_DNS"
-$SUDO ssh -o StrictHostKeyChecking=no ubuntu@"$EC2_PUBLIC_DNS"  "sudo systemctl stop test-app.service"
-echo "stopped test-app service"
-$SUDO cat "$ZIP_DIR" | "$SUDO" ssh -o StrictHostKeyChecking=no "$SSH_LOGIN" "sudo cat > $ZIP_DIR"
-echo "copied zip to ec2 instance"
-$SUDO ssh -o StrictHostKeyChecking=no "$SSH_LOGIN" "sudo unzip -o $ZIP_DIR -d /opt/"
-echo "unzipped zip to /opt/"
-$SUDO ssh -o StrictHostKeyChecking=no "$SSH_LOGIN"  "sudo systemctl start test-app.service"
-echo "started test-app service"
+$SUDO sleep 10
+echo "Added ingress rule to aws"
